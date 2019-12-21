01fae14838c5a15c4988fe2e0c217640de560180
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-12-21 11:36:46 +0900

    service追加

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 
 import { HelloService } from './services/hello.service';
+import { SysDateService } from './services/sysdate.service';
 
 @Controller()
 export class AppController {
@@ -11,5 +12,11 @@ export class AppController {
   getHello(): string {
     const service = this.moduleRef.get('HELLO_SERVICE') as HelloService;
     return service.getHello();
+  }
+
+  @Get('sysdate')
+  getSysDate(): string {
+    const service = this.moduleRef.get('SYSDATE_SERVICE') as SysDateService;
+    return service.getDate();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { HelloService } from './services/hello.service';
+import { SysDateService } from './services/sysdate.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [{ provide: 'HELLO_SERVICE', useClass: HelloService }],
+  providers: [
+    { provide: 'HELLO_SERVICE', useClass: HelloService },
+    { provide: 'SYSDATE_SERVICE', useClass: SysDateService },
+  ],
 })
 export class AppModule {}
